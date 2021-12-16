3e44e6f6233228437ed0a2197bdc8ada07f2322e
Alex <91600207+abailone93@users.noreply.github.com>
2021-12-16 11:28:32 +0100

    update_16-12-2021

--- controladorSesiones.php
+++ controladorSesiones.php
@@ -6,8 +6,7 @@
     session_unset();
     //destruye la session
     session_destroy();
-    $cookie_nombre = "usuario";
-    $cookie_valor = "Alex Bailon";
-    setcookie($cookie_nombre, $cookie_valor, time() + (30 + 30), "/); //60 segundos
-    
+
+
+    //60 segundos
 ?>
